50ea019121ddbc70f41df80da429745ed6cfe803
IvanDirigible <sickeruncible@gmail.com>
2024-01-24 16:23:00 -0500

    Removed TODO comments. Tested and removed redundant code.

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,4 +1,4 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
@@ -26,7 +26,6 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
 registerRoute(
   ({ request }) => {
     console.log(request);
@@ -59,20 +58,3 @@ registerRoute(
     ],
   })
 );
-
-// registerRoute(
-//   ({ request }) => {
-//     console.log(request);
-//     return (
-//       request.destination === 'style' || request.destination === 'script' || request.destination === 'worker'
-//     );
-//   },
-//   new StaleWhileRevalidate({
-//     cacheName: 'static-resources',
-//     plugins: [
-//       new CacheableResponsePlugin({
-//         statuses: [0, 200],
-//       }),
-//     ],
-//   })
-// );
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-// export const putDb = async (content) => console.error('putDb not implemented');
 export const putDb = async (content) => {
   console.log('PUT to the database.');
   const jateDB = await openDB('jate', 1);
@@ -24,8 +22,6 @@ export const putDb = async (content) => {
   console.log('Data saved to the database.', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-// export const getDb = async () => console.error('getDb not implemented');
 export const getDb = async () => {
   console.log('GET from the database.');
   const jateDB = await openDB('jate', 1);
@@ -34,7 +30,7 @@ export const getDb = async () => {
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
-  return result?.value;
+  return result.value;
 };
 
 initdb();
