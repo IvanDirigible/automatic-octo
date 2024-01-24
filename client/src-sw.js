78b3ad2f7c1f739ae2c7abfa24139ca695907305
IvanDirigible <sickeruncible@gmail.com>
2024-01-24 13:53:12 -0500

    Issue with build. Adjusted database.

--- client/src-sw.js
+++ client/src-sw.js
@@ -43,3 +43,19 @@ registerRoute(
     ],
   })
 );
+
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'jate-image-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 14 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
