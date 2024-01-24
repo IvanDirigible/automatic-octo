47d1443aa7781dd538ab29b57aa94321b299cb8f
IvanDirigible <sickeruncible@gmail.com>
2024-01-24 14:10:07 -0500

    Issues with assets.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,7 +27,7 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 // export const getDb = async () => console.error('getDb not implemented');
 export const getDb = async () => {
-  console.log('GET all from the database.');
+  console.log('GET from the database.');
   const jateDB = await openDB('jate', 1);
   const tx =jateDB.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
